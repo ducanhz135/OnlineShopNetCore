5a56f0e3975b15a555f2e95a0f1b15053f5aef36
duc anh <ducanhz135@gmail.com>
2020-05-05 21:51:53 +0700

    application structure

---
+++ eShopSolution/eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IManageProductService
+    {
+        //Task<int> Create(ProductCreateRequest request);
+
+        //Task<int> Update(ProductUpdateRequest request);
+
+        //Task<int> Delete(int productId);
+
+        //Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
+    }
+}
---
+++ eShopSolution/eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShopSolution.Application.Catalog.Products
+{
+    public interface IPublicProductService
+    {
+        
+
+    }
+}
